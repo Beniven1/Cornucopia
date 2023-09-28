26858f12b1563ace875937ad99a244569bd28c2c
Beniven1 <Benc3557@gmail.com>
2023-09-27 17:34:49 -0700

    made mod

---
+++ src/main/java/beniven/mods/cornucopia/cornucopia.java
@@ -0,0 +1,8 @@
+package beniven.mods.cornucopia;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(cornucopia.MODID)
+public class cornucopia {
+    public static final String MODID = "cornucopia";
+}
